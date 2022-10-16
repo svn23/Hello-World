dd2887559314f6ec5370a4d6ced3a3b08d3f892c
Sovan Sen <102777649+svn23@users.noreply.github.com>
2022-10-16 12:46:53 +0530

    password auth.py

---
+++ password auth.py
@@ -0,0 +1,26 @@
+employees = {}
+
+max_length = 5
+
+while len(employees) < max_length:
+    u = input("Enter employee's name: ")
+    p = input("Enter employee's Password: ")
+
+    employees[u] = p
+
+
+# 👇️ {'Alice': '100', 'Bob': '100', 'Carl': '100'}
+print(employees)
+
+print('Enter correct username and password combo to continue')
+count=0
+while count < 3:
+    a = input ("Enter correct username to continue: ")
+    b = input ("Enter correct password to continue: ")
+
+    if b ==list(employees.values())[0] and a ==list(employees.keys())[0]:
+        print('Access granted')
+        break
+    else:
+        print('Access denied. Try again.')
+        count += 1
