18e806a79dd945d452e44c3b49aa4ba4298d32a1
Sovan Sen <102777649+svn23@users.noreply.github.com>
2022-10-16 13:03:49 +0530

    Password authentication with 5 time retry attempts

--- password auth.py
+++ password auth.py
@@ -4,7 +4,6 @@ Working Model >
     Then It will ask for the username and password for the login
     Then it will check the username and password for the authentication
     At last it will show you the desired Output"""
-
 employees = {}
 
 max_length = 5
