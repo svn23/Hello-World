70258ca9653755b5fc0dde71d523c2cbad7531b8
Sovan Sen <102777649+svn23@users.noreply.github.com>
2022-10-16 13:01:00 +0530

    password auth.py

--- password auth.py
+++ password auth.py
@@ -1,3 +1,10 @@
+"""This Code is for testing purposes
+Working Model >
+    First of all % username % password will be stored in the dictionary
+    Then It will ask for the username and password for the login
+    Then it will check the username and password for the authentication
+    At last it will show you the desired Output"""
+
 employees = {}
 
 max_length = 5
